46fa513ad417c597c41f3317c14a54a6967dc404
zao95 <jeongwoo@ifgroup.io>
2022-08-22 11:50:44 +0900

    ENH: enhance setting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import 'hardhat-gas-reporter'
+import 'dotenv/config'
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
-};
+    solidity: {
+        version: '0.8.16',
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 10,
+            },
+        },
+    },
+    gasReporter: {
+        enabled: process.env.GAS_REPORT ? true : false,
+    },
+    defaultNetwork: process.env.DEFAULT_NETWORK || 'hardhat',
+}
 
-export default config;
+export default config
